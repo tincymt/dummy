ea4151500d914c595920facc4258939fdd091b67
tincymt <50576155+tincymt@users.noreply.github.com>
2021-07-02 19:49:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+//$con=mysqli_connect("localhost","root","","dancedb") or die("couldn't connect");
+  $con=mysqli_connect("remotemysql.com","15WDYv6YEw","i20LUHOpjE","15WDYv6YEw") or die("couldn't connect");
+?>
